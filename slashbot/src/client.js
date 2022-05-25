eebee5065f606b6fd23ac221520e24710036448c
uh! <76551996+clqu@users.noreply.github.com>
2022-05-25 18:39:22 +0300

    Update client.js

--- slashbot/src/client.js
+++ slashbot/src/client.js
@@ -27,17 +27,6 @@ module.exports = class ClquClient {
     }
 
     init() {
-
-        function $(label) {
-            if (this?.fields[label]) {
-                return this.fields[label];
-            } else if (moduleConfig.fields[label]) {
-                return moduleConfig.fields[label];
-            } else {
-                return label;
-            }
-        };
-
         //------------------------------------------------------------------------------------//
 
         const rest = new REST({ version: "9" }).setToken(this.token);
@@ -52,8 +41,6 @@ module.exports = class ClquClient {
             })();
         });
 
-        // =================================================================================== \\
-
         this.client.on("interactionCreate", async (interaction) => {
             if (!interaction.isCommand()) return;
             let $command = this.commands.find(e => e.name === interaction.commandName);
@@ -61,6 +48,7 @@ module.exports = class ClquClient {
                 $command.run(this.client, interaction)
             }
         });
+        
         //------------------------------------------------------------------------------------//
 
         this.client.on('ready', () => {
